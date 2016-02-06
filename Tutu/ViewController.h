0862613cfcb1bcba46eb41f39908d7f6d62fa74a
Yulia Kashirina <kashirinayi@gmail.com>
2016-02-06 15:00:34 +0300

    initial commit

---
+++ Tutu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tutu
+//
+//  Created by Artyom Ryzhkov on 06/02/16.
+//  Copyright © 2016 Yulia Kashirina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tutu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tutu
+//
+//  Created by Artyom Ryzhkov on 06/02/16.
+//  Copyright © 2016 Yulia Kashirina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
